d85fb5c74116d87d262fe896c584ca1c4b06043b
vaishnavireddy0008 <vaishnavireddy.0008@gmail.com>
2025-01-04 11:28:41 +0530

    Add files via upload

---
+++ demo8.java
@@ -0,0 +1,25 @@
+import java.util.scanner;
+
+public class demo8 {
+
+    public static void main(string[] args) {
+        Scanner sc = new scanner(System.in);
+        String loginid;
+        String pwd;
+        String reply;
+        do {
+        System.out.println(x:"welcome to malla reddy university");
+        System.out.println(x:"have you visited your site earlier");
+        reply = sc.nextLine();
+        if(reply == "no") {
+            System.out.println(x:"login here");
+            System.out.println(x:"enter ur id an pwd");
+            loginid = sc.nextLine();
+            pwd = sc.nextLine();
+        }else
+        System.out.println(x:"you are our valued customer");
+    } while (true);
+
+}
+    }
+}
