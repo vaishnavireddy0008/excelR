496e8652a62bc21cc2b957186338e385a90563dd
vaishnavireddy0008 <vaishnavireddy.0008@gmail.com>
2025-02-12 09:47:01 +0530

    Add files via upload

---
+++ Demo066.java
@@ -0,0 +1,8 @@
+class Demo066 {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7};
+        for(int i =0;i<arr.length;i++){
+            System.out.println(i);
+        }
+    }
+}
