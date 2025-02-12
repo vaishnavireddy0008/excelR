97448d9d8b27031ba1a531e5d2ea3fe683be01eb
vaishnavireddy0008 <vaishnavireddy.0008@gmail.com>
2025-02-12 09:44:49 +0530

    Add files via upload

---
+++ Demo062.java
@@ -0,0 +1,14 @@
+public class Demo062 {
+    public static void main(String[] args) {
+        
+        int[] array = {1, 2, 3, 4, 5};
+        
+        System.out.println("1st element: " + array[0]);
+        
+        
+        System.out.println("2nd element: " + array[1]);
+        
+        
+        System.out.println("Last element: " + array[array.length - 1]);
+    }
+}
